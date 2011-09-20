65cf610dc24a7cf90212d043254c8eea1ace3f5a
Chris Fontenot <chris.fontenot@gmail.com>
2011-09-20 12:53:09 -0600

    first commit

---
+++ src/main/java/com/sungardhe/demo/portlets/helloportlet/HelloPortlet.java
@@ -0,0 +1,22 @@
+package com.sungardhe.demo.portlets.helloportlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.portlet.GenericPortlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+public class HelloPortlet extends GenericPortlet {
+
+	@Override
+	protected void doView(RenderRequest request, RenderResponse response)
+			throws PortletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<b>Hello, World!</b><br/>This is a great thing!");
+		
+	
+	}
+	
+}
